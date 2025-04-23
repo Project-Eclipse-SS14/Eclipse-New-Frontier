e10368132fd6a450eec8711f2ed0b88f09af4bea
FoLZer <65248062+FoLZer@users.noreply.github.com>
2025-04-23 14:06:22 +0200

    Attempt to fix gravity on load

--- Content.Server/Gravity/GravityGeneratorComponent.cs
+++ Content.Server/Gravity/GravityGeneratorComponent.cs
@@ -1,12 +1,11 @@
 using Content.Shared.Gravity;
-using Content.Shared.Construction.Prototypes;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Serialization;
 
 namespace Content.Server.Gravity
 {
     [RegisterComponent]
     [Access(typeof(GravityGeneratorSystem))]
-    public sealed partial class GravityGeneratorComponent : SharedGravityGeneratorComponent
+    public sealed partial class GravityGeneratorComponent : SharedGravityGeneratorComponent, ISerializationHooks
     {
         [DataField("lightRadiusMin")] public float LightRadiusMin { get; set; }
         [DataField("lightRadiusMax")] public float LightRadiusMax { get; set; }
@@ -16,5 +15,34 @@ namespace Content.Server.Gravity
         /// </summary>
         [ViewVariables]
         public bool GravityActive { get; set; } = false;
+
+        void ISerializationHooks.AfterDeserialization()
+        {
+            var entityManager = IoCManager.Resolve<EntityManager>();
+            if (!entityManager.Initialized)
+            {
+                return;
+            }
+            try
+            {
+                // No way to check if EntitySysManager is initialized directly, so we're checking it this way
+                var _ = entityManager.EntitySysManager.DependencyCollection;
+            }
+            catch (InvalidOperationException)
+            {
+                return;
+            }
+            var gravitySystem = entityManager.SystemOrNull<GravitySystem>();
+            if (gravitySystem == null)
+            {
+                return;
+            }
+            var xform = entityManager.TransformQuery.GetComponent(Owner);
+            if (xform == null)
+            {
+                return;
+            }
+            gravitySystem.RefreshGravity(xform.ParentUid);
+        }
     }
 }
