22580527fa94b520aa941573bf34486ee93a8803
FoLZer <65248062+FoLZer@users.noreply.github.com>
2025-04-28 19:04:27 +0200

    Add script to detect not translated files

---
+++ Tools/better_ss14_ru/detect_not_translated/src/main.rs
@@ -0,0 +1,35 @@
+use std::path::PathBuf;
+
+use common::prototypes_ftl::{FtlStorage, StringOrOtherId};
+use simple_logger::SimpleLogger;
+
+const SAVE_LOCALE_PATH: &str = "../../Resources/Locale/ru-RU/ss14-ru-better/";
+
+fn main() {
+    SimpleLogger::new()
+        .with_level(log::LevelFilter::Info)
+        .init()
+        .unwrap();
+
+        let save_locale_path = &PathBuf::from(SAVE_LOCALE_PATH);
+
+        let mut ftl_project = FtlStorage::load(save_locale_path.clone()).unwrap();
+
+        for (_, file) in &mut ftl_project.files {
+            for prototype in &mut file.prototypes {
+                let mut is_translated = true;
+                if let Some(StringOrOtherId::String(v)) = prototype.name() {
+                    is_translated = is_translated && v.contains(|c: char| !c.is_ascii());
+                }
+                if let Some(StringOrOtherId::String(v)) = prototype.description() {
+                    is_translated = is_translated && v.contains(|c: char| !c.is_ascii());
+                }
+                if let Some(StringOrOtherId::String(v)) = prototype.suffix() {
+                    is_translated = is_translated && v.contains(|c: char| !c.is_ascii());
+                }
+                if !is_translated {
+                    log::info!("Prototype id {{ ent-{} }} is not translated!", prototype.id);
+                }
+            }
+        }
+}
